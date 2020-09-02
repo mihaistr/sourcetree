a0bf4bc559a4cfc416e50e05c9b353ce77a489d3
Mihai Stroila <stroila.mihailiviu@gmail.com>
2020-09-03 01:54:43 +0300

    5

--- Source.cpp
+++ Source.cpp
@@ -9,11 +9,9 @@ uint8_t set(uint8_t number, uint8_t position) {
     return (number | (1u << position));
 }
 
-
 uint8_t unset(uint8_t number, uint8_t position) {
     return (number & (~(1u << position)));
 }
-
 
 uint8_t read_one_bit(uint8_t number, uint8_t position) {
     return ((number & (1 << position)) ? SET : NOT_SET);
@@ -23,19 +21,14 @@ uint8_t toggle_one_bit(uint8_t number, uint8_t position) {
     return (n ^ (1u << position));
 }
 
-
-
 uint8_t bit_insertion(uint8_t number, uint8_t number_to_insert, uint8_t position) {
     return (number | (number_to_insert << position));
 }
 
-uint8_t bit_extraction(uint8_t number, uint8_t number_to_extract, uint8_t position) {///////////////////////////////////////////////////////////
-    uint8_t mask = ~0b0;
-    return (number & ()
-}
-
-
-
+//uint8_t bit_extraction(uint8_t number, uint8_t number_to_extract, uint8_t position) {////////////////////////TO BE CONTINUED///////////////////////////////////
+//    uint8_t mask = ~0b0;
+//    return (number & ()....................
+//}
 
 uint8_t count_leading_Zeros(uint8_t number)
 {
@@ -70,13 +63,14 @@ uint8_t count_leading_Ones(uint8_t number)
 uint8_t set_rightmost_zero_to_one(uint8_t number) {
     if (number == 0) return 1;   // daca numarul este 0 
     return (number | (number + 1));
+
 }
 
 uint8_t set_rightmost_one_to_zero(uint8_t number) {
     if (number == 0) return 0;
     return (number & (number - 1));
-}
 
+}
 
 uint8_t set_leftmost_zero_to_one(uint8_t number) {
     int position = 0;
@@ -88,11 +82,18 @@ uint8_t set_leftmost_zero_to_one(uint8_t number) {
 
     return (number | (1 << position));
 
-
 }
 
 uint8_t set_leftmost_one_to_zero(uint8_t number) {
+    int position;
+
     if (number == 0) return 0;
+    // gaseste pozitia celui mai din stanga 0 bit 
+    for (int number2 = number, count = 0; number2 > 0; number2 >>= 1, count++) {
+        if ((number2 & 1) == 1) position = count;
+    }
+
+    return (number ^ (1 << position));
 
 }
 

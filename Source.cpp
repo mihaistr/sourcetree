dbfb7f9553dec35c7074d4c2c3a0cda9fd357689
Mihai Stroila <stroila.mihailiviu@gmail.com>
2020-09-03 01:33:09 +0300

    4

--- Source.cpp
+++ Source.cpp
@@ -68,9 +68,8 @@ uint8_t count_leading_Ones(uint8_t number)
 }
 
 uint8_t set_rightmost_zero_to_one(uint8_t number) {
-    if (number == 0) return 1;
+    if (number == 0) return 1;   // daca numarul este 0 
     return (number | (number + 1));
-
 }
 
 uint8_t set_rightmost_one_to_zero(uint8_t number) {
@@ -78,22 +77,23 @@ uint8_t set_rightmost_one_to_zero(uint8_t number) {
     return (number & (number - 1));
 }
 
-uint8_t setRightmostUnsetBit(uint8_t number)
-{
 
-    if (number == 0)
-        return 1;
+uint8_t set_leftmost_zero_to_one(uint8_t number) {
+    int position = 0;
 
-    // if all bits of 'n' are set 
-    if ((number & (number + 1)) == 0)
-        return number;
+    // gaseste pozitia celui mai din stanga 0 bit 
+    for (int number2 = number, count = 0; number2 > 0; number2 >>= 1, count++) {
+        if ((number2 & 1) == 0) position = count;
+    }
 
-    // position of rightmost unset bit in 'n' 
-    // passing ~n as argument 
-    int pos = ~(log2(number & -number) + 1);
+    return (number | (1 << position));
 
-    // set the bit at position 'pos' 
-    return ((1 << (pos - 1)) | number);
+
+}
+
+uint8_t set_leftmost_one_to_zero(uint8_t number) {
+    if (number == 0) return 0;
+
 }
 
 
